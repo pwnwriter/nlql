40fc007f81e3a7520a0016247e6db74a0693bf46
pwnwriter <hi@pwnwriter.me>
2026-02-17 19:53:24 -0500

    chore(flake): add support for nix build and run

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
 {
+  description = "nlql - Talk to your database in plain English";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, rust-overlay, ... }:
     let
       systems = [
         "x86_64-linux"
@@ -13,21 +19,56 @@
         "aarch64-darwin"
       ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system (import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      }));
+
+      mkPackage = system: pkgs:
+        let
+          inherit (pkgs) lib stdenv;
+        in
+        pkgs.rustPlatform.buildRustPackage {
+          pname = "nlql";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ] ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+          meta = with lib; {
+            description = "Talk to your database in plain English using AI";
+            homepage = "https://github.com/pwnwriter/nlql";
+            license = licenses.mit;
+            mainProgram = "nlql";
+          };
+        };
     in
     {
-      devShells = forAllSystems (pkgs: {
-        default = import ./nix/shell.nix { inherit pkgs; };
+      packages = forAllSystems (system: pkgs: {
+        default = mkPackage system pkgs;
+        nlql = mkPackage system pkgs;
       });
 
-      checks = forAllSystems (pkgs: {
-        devShell = pkgs.mkShell {
-          packages = [
-            # pkgs.rustc
-            # pkgs.cargo
-
-          ];
+      apps = forAllSystems (system: pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/nlql";
         };
+      });
+
+      devShells = forAllSystems (system: pkgs: {
+        default = import ./nix/shell.nix { inherit pkgs; };
       });
     };
 }
